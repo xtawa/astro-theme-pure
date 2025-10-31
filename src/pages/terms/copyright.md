3dbd68d09168666a7c3dd2b81290fc449a9a5798
Xihao Zhang <zeromostia@gmail.com>
2025-10-31 23:21:35 +0800

    first commit

---
+++ src/pages/terms/copyright.md
@@ -0,0 +1,13 @@
+---
+layout: '@/layouts/IndividualPage.astro'
+
+title: '版权声明'
+description: '生效日期: 2024-11-26'
+language: '简体中文'
+back: '/terms/list'
+---
+***以简体中文作为标准语言，翻译时若出现差错，以简体中文结果为准。***
+
+本站所有文章，除特别表明外，均为本人原创。
+
+允许转载，请注明来源。
---
+++ src/site.config.ts
@@ -0,0 +1,196 @@
+import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } from 'astro-pure/types'
+
+export const theme: ThemeUserConfig = {
+  // === Basic configuration ===
+  /** Title for your website. Will be used in metadata and as browser tab title. */
+  title: "YanLuo洛 · 此生拾穗",
+  /** Will be used in index page & copyright declaration */
+  author: 'Coisini_Luo',
+  /** Description metadata for your website. Can be used in page metadata. */
+  description: '存在于世间的记忆',
+  /** The default favicon for your site which should be a path to an image in the `public/` directory. */
+  favicon: 'favicon/favicon.ico',
+  /** Specify the default language for this site. */
+  locale: {
+    lang: 'zh-CN',
+    attrs: 'zh_CN',
+    // Date locale
+    dateLocale: 'zh-CN',
+    dateOptions: {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    }
+  },
+  /** Set a logo image to show in the homepage. */
+  logo: {
+    src: 'src/assets/avatar.png',
+    alt: 'Avatar'
+  },
+
+  // === Global configuration ===
+  titleDelimiter: '•',
+  prerender: true,
+  npmCDN: 'https://cdn.jsdelivr.net/npm',
+
+  // Still in test
+  head: [
+    /* Telegram channel */
+    // {
+    //   tag: 'meta',
+    //   attrs: { name: 'telegram:channel', content: '@cworld0_cn' },
+    //   content: ''
+    // }
+  ],
+  customCss: [],
+
+  /** Configure the header of your site. */
+  header: {
+    menu: [
+      { title: '主页', link: 'https://xtyin.com' },
+      { title: '归档', link: '/archives' },
+      { title: '标签', link: '/tags' },
+      { title: '项目', link: '/projects' },
+      { title: '友链', link: '/links' },
+      { title: '关于', link: '/about' }
+    ]
+  },
+
+  /** Configure the footer of your site. */
+  footer: {
+    // Year format
+    year: `© ${new Date().getFullYear()}`,
+    // year: `© 2019 - ${new Date().getFullYear()}`,
+    links: [
+      // Registration link
+      {
+        title: 'Moe ICP(wait)',
+        link: 'https://icp.gov.moe/?keyword=',
+        style: 'text-sm' // Uno/TW CSS class
+      },
+      {
+        title: 'Travelling',
+        link: 'https://www.travellings.cn/go.html',
+        style: 'text-sm'
+      },
+      // Privacy Policy link
+      {
+        title: 'Site Policy',
+        link: '/terms/list',
+        pos: 2 // position set to 2 will be appended to copyright line
+      }
+    ],
+    /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
+    credits: true,
+    /** Optional details about the social media accounts for this site. */
+    social: { github: 'https://github.com/xtawa' }
+  },
+
+  content: {
+    /** External links configuration */
+    externalLinks: {
+      content: ' ↗',
+      /** Properties for the external links element */
+      properties: {
+        style: 'user-select:none'
+      }
+    },
+    /** Blog page size for pagination (optional) */
+    blogPageSize: 8,
+    // Currently support weibo, x, bluesky
+    share: ['x']
+  }
+}
+
+export const integ: IntegrationUserConfig = {
+  // Links management
+  // See: https://astro-pure.js.org/docs/integrations/links
+  links: {
+    // Friend logbook
+    logbook: [
+      { date: '2025-10-31', content: '欢迎您提交友链' },
+      { date: '2025-03-16', content: "Feel free to apply the link" }
+    ],
+    // Yourself link info
+    applyTip: [
+      { name: 'Name', val: theme.title },
+      { name: 'Desc', val: theme.description || 'Null' },
+      { name: 'Link', val: 'https://blog.xtyin.com' },
+      { name: 'Avatar', val: 'https://blog.xtyin.com/favicon1.ico' }
+    ]
+  },
+  // Enable page search function
+  pagefind: true,
+  // Add a random quote to the footer (default on homepage footer)
+  // See: https://astro-pure.js.org/docs/integrations/advanced#web-content-render
+  quote: {
+    // https://developer.hitokoto.cn/sentence/#%E8%AF%B7%E6%B1%82%E5%9C%B0%E5%9D%80
+    // server: 'https://v1.hitokoto.cn/?c=i',
+    // target: (data) => (data as { hitokoto: string }).hitokoto || 'Error'
+    // https://github.com/lukePeavey/quotable
+    server: 'https://api.quotable.io/quotes/random?maxLength=60',
+    target: `(data) => data[0].content || 'Error'`
+  },
+  // UnoCSS typography
+  // See: https://unocss.dev/presets/typography
+  typography: {
+    class: 'prose text-base text-muted-foreground',
+    // The style of blockquote font, normal or italic (default to italic in typography)
+    blockquoteStyle: 'italic',
+    // The style of inline code block, code or modern (default to code in typography)
+    inlineCodeBlockStyle: 'modern'
+  },
+  // A lightbox library that can add zoom effect
+  // See: https://astro-pure.js.org/docs/integrations/others#medium-zoom
+  mediumZoom: {
+    enable: true, // disable it will not load the whole library
+    selector: '.prose .zoomable',
+    options: {
+      className: 'zoomable'
+    }
+  },
+  // Comment system
+  waline: {
+    enable: true,
+    // Server service link
+    server: 'https://waline.xtyin.com/',
+    // Refer https://waline.js.org/en/guide/features/emoji.html
+    emoji: ['bmoji', 'weibo'],
+    // Refer https://waline.js.org/en/reference/client/props.html
+    additionalConfigs: {
+      // search: false,
+      pageview: true,
+      comment: true,
+      locale: {
+        reaction0: 'Like',
+        placeholder: 'Welcome to comment. (Email to receive replies. Login is unnecessary)'
+      },
+      imageUploader: false
+    }
+  }
+}
+
+export const terms: CardListData = {
+  title: 'Terms content',
+  list: [
+    {
+      title: 'Privacy Policy',
+      link: '/terms/privacy-policy'
+    },
+    {
+      title: 'Terms and Conditions',
+      link: '/terms/terms-and-conditions'
+    },
+    {
+      title: 'Copyright',
+      link: '/terms/copyright'
+    },
+    {
+      title: 'Disclaimer',
+      link: '/terms/disclaimer'
+    }
+  ]
+}
+
+const config = { ...theme, integ } as Config
+export default config
